3f6d4dfd1780bad1a3b3b02901fdaa67656a4f89
Brandon Gonzalez <brajgon@gmail.com>
2025-01-08 23:45:03 -0700

    minor changes again

--- src/components/IncomeTracker.jsx
+++ src/components/IncomeTracker.jsx
@@ -109,13 +109,13 @@ export const IncomeTracker = () => {
             onClick={handleAddRow}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Add Another Expense
+            Add
           </button>
           <button
             type="submit"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Save Income
+            Save
           </button>
           <button
             type="button"

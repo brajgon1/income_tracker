ef65c763f721b90a40c4e9c0ef95de05a38f2ccb
Brandon Gonzalez <brajgon@gmail.com>
2025-01-23 20:25:49 -0700

    messing around

--- src/components/ColeInvite.jsx
+++ src/components/ColeInvite.jsx
@@ -1,23 +1,22 @@
 export default function ColeInvite() {
   return (
-    <div className="min-h-screen font-poppins bg-gradient-to-r from-cyan to-transparent h-32 w-full p-6">
-      <h1 className="text-2xl font-poppins font-bold text-center mb-4">
-        Cole's Invite
-      </h1>
-      <div className="p-6 rounded-sm max-w-3xl mx-auto mb-6 mt-4">
-        <p className="text-center">
-          You are invited to Cole's birthday party! 🎉
-        </p>
-        <br />
-        <p className="text-center">April 12, 2025</p>
-        <br />
-        <p className="text-center">3:00 PM - 5:00 PM</p>
-        <br />
-        <p className="text-center">1234 Elm Street</p>
+      <div className="min-h-screen font-poppins bg-gradient-to-l from-cyan to-transparent h-32 w-full p-6">
+        <h1 className="text-2xl font-poppins font-bold text-center mb-4">
+          Cole's Invite
+        </h1>
+        <div className="p-6 rounded-sm max-w-3xl mx-auto mb-6 mt-4">
+          <p className="text-center">
+            You are invited to Cole's birthday party! 🎉
+          </p>
+          <br />
+          <p className="text-center">April 12, 2025</p>
+          <br />
+          <p className="text-center">3:00 PM - 5:00 PM</p>
+          <br />
+          <p className="text-center">1234 Elm Street</p>
+        </div>
       </div>
-    </div>
   );
 }
-
 
 // google RSVP form examples for inspiration
